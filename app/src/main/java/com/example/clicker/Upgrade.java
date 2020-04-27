201807a2f3771afd2f68e90ae77df8c6ee65d68b
MeeLock <zalipatr1@yandex.ru>
2020-04-27 08:44:09 +0300

    little code-view

--- app/src/main/java/com/example/clicker/Upgrade.java
+++ app/src/main/java/com/example/clicker/Upgrade.java
@@ -7,25 +7,21 @@ public class Upgrade {
     private double Multiplier = 1.07;
     //Todo: обычные типы не подходят нужны биг десимал может и биг инт
     private String name;
-    private int lvl = 0;
+    private int lvl;
     private double speed;
     private int basePrice;
 
     @NonNull
     @Override
     public String toString() {
-        StringBuilder str = new StringBuilder();
-
-        str.append("\n");
-        str.append("name: ").append(this.name).append("\n");
-        str.append("speed: ").append(this.speed).append("\n");
-        str.append("price: ").append(this.basePrice).append("\n");
-        str.append("lvl: ").append(this.lvl);
-
-        return str.toString();
+        return "\n" +
+                "name: " + this.name + "\n" +
+                "speed: " + this.speed + "\n" +
+                "price: " + this.basePrice + "\n" +
+                "lvl: " + this.lvl;
     }
 
-    public Upgrade (String name, int price, double speed, int lvl) {
+    Upgrade(String name, int price, double speed, int lvl) {
         this.name = name;
         this.speed = speed;
         this.basePrice = price;
@@ -41,18 +37,18 @@ public class Upgrade {
     }
 
     public String getName() { return this.name; }
-    public int getLVL() { return this.lvl; }
+    int getLVL() { return this.lvl; }
 //    public double getSpeed() { return this.speed; }
-    public int getBasePrice() { return this.basePrice; }
+    int getBasePrice() { return this.basePrice; }
 
-    public double getTotalSpeed() {
+    double getTotalSpeed() {
         if(lvl == 0)
             return speed;
         else
             return MainActivity.roundAvoid(speed * lvl, 2);
     }
 
-    public int getTotalPrice() {
+    int getTotalPrice() {
         if(lvl == 0)
             return basePrice;
         else
